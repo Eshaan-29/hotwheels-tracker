a02a5a61251977eab6ee46c769e95042d28c5127
Eshaan Satyawan Mali <eshaanmali18@gmail.com>
2026-02-28 23:18:59 +0530

    Correct capitalization of 'Futurismo' in car list

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ TARGET_CARS = [
     "mcmurthy", "mcmurtry", "nissan", "mazda", "f1", 
     "premium", "toyota", "lamborgini", "lamborghini",
     "ferrari", "gordon murray", "barbie", "batman", 
-    "batmobile", "datsun", "pagani", "mclaren","futurismo"
+    "batmobile", "datsun", "pagani", "mclaren","Futurismo"
 ]
 
 # 🎭 THE DISGUISE CLOSET
