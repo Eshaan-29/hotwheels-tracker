6bf3c94c12a6850504f9d4208777f23b40bd68da
Eshaan Satyawan Mali <eshaanmali18@gmail.com>
2026-02-28 23:09:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ TARGET_CARS = [
     "mcmurthy", "mcmurtry", "nissan", "mazda", "f1", 
     "premium", "toyota", "lamborgini", "lamborghini",
     "ferrari", "gordon murray", "barbie", "batman", 
-    "batmobile", "datsun", "pagani", "mclaren"
+    "batmobile", "datsun", "pagani", "mclaren","futorismo"
 ]
 
 # 🎭 THE DISGUISE CLOSET
